e6c94eaf7a5a232fa6313ce9cf49e2d5e7d77da3
wtf <wtf@wtf>
2021-11-01 10:37:23 +0400

    创建表结构成功-nice---报错：1050 --迁移数据库表报错 1.使用命令 python manage.py migrate --fake 先进行表结构的同步
    2.完成后删除make_migrations表中最新的迁移数据,在进行迁移即可

--- meiduo_mall/meiduo_mall/apps/orders/url s.py
+++ meiduo_mall/meiduo_mall/apps/orders/url s.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     # 结算订单
     url(r'^order/settlement/$', views.OrderSettlementView.as_view(), name='settlement'),
+    # 提交订单
+    url(r'^orders/commit/$', views.OrderCommitView.as_view()),
+    # 提交订单成功
+    url(r'^orders/success/$', views.OrderSuccessView.as_view()),
 ]
